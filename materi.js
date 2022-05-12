a235e60a85875831918760a085aa7e9bd9f59fc5
Jody-septiawan <jody.septiawan5@gmail.com>
2022-05-12 10:48:36 +0700

    First

---
+++ materi.js
@@ -0,0 +1,53 @@
+// Variabel
+
+// let
+let name = 'Alan Sadewa'; // string
+
+let address = 'Wonosobo';
+address = 'Kuningan';
+
+let age = 21; // number / integer
+
+let a = '10';
+let b = 3;
+
+let c = a + b;
+
+// console.log(typeof a);
+
+// const
+const kelas = 'FullStack';
+// kelas = 'DevOps';
+
+const Company = 'Dumbways Indonesia';
+
+// var
+
+// Function ----------------------------
+function perkalian(x, y) {
+  const bil1 = x;
+  const bil2 = y;
+
+  let result = bil1 * bil2;
+
+  //   console.log(bil1 + ' * ' + bil2 + ' = ' + result);
+  console.log(`${bil1} * ${bil2} = ${result}`);
+}
+
+function penjumlahan(x, y) {
+  const bil1 = x;
+  const bil2 = y;
+
+  let result = bil1 + bil2;
+
+  //   console.log(bil1 + ' * ' + bil2 + ' = ' + result);
+  console.log(`${bil1} + ${bil2} = ${result}`);
+}
+
+// penjumlahan(90, 4);
+// perkalian(90, 4);
+
+const hasil = 9.3 + 2;
+
+console.log(hasil);
+console.log(typeof hasil);
